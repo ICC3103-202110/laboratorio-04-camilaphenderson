8cc5d171ac0a8e58bd31e8cb1b630b068b1dab18
camilaphenderson <cvpellegrini@miuandes.cl>
2021-05-08 18:43:44 -0400

    Listo

--- Paso3.js
+++ Paso3.js
@@ -3,18 +3,17 @@ var prompt = require('prompt-sync')();
 
 
 function app(counter){
-    while(True){
+    while(true){
         const currentView = view(counter)
         console.clear()
         console.log(currentView)
-        const msg = prompt('\n\n(+) or (-)\n\n(q) to quit \n\nWhat would you do? ')
+        const msg = prompt('\n\n(+) or (-)\n\n(q) to quit \n\nWhat would you like to do? ')
         if (msg === q){
             console.log('You quit')
             break
         }
         else {
             counter = update(msg,counter)
-            app(counter)
         }
     }
 }
